3213935ac7e03c6d2e69a26f26feeb0d095772c9
vahaponuryildirim@gmail.com <vahaponuryildirim@gmail.com>
2023-09-04 09:06:09 +0300

    response helpers added

---
+++ response_helpers.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.WriteHeader(status)
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+	w.Write(data)
+}
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	respondWithJSON(w, code, err.Error())
+}
