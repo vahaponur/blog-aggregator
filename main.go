817c5b5d20eaba2ef0cfea56434722c701f31d1a
vahaponuryildirim@gmail.com <vahaponuryildirim@gmail.com>
2023-09-05 10:21:40 +0300

    gracefull shutdown

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	_ "github.com/lib/pq"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var cfg *Config
@@ -12,6 +17,26 @@ func main() {
 	cfg = createConfig()
 	mainRouter := createRouter()
 	http.Handle("/", mainRouter)
-	fmt.Println(fmt.Sprintf("Server started at port:%v", cfg.env.PORT))
-	http.ListenAndServe(cfg.env.PORT, nil)
+
+	server := &http.Server{Addr: cfg.env.PORT}
+	fmt.Println("Starting the server...")
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}()
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	// Wait for signals
+	<-signalChan
+	fmt.Println("\nReceived interrupt signal. Shutting down gracefully...")
+
+	// Shutdown the server gracefully
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Error during server shutdown: %s\n", err)
+	}
+
 }
